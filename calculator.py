51a266d6b5b15f5b57b6deae34f23619264dd50b
nikita74939 <itanikita20005@gmail.com>
2025-08-09 11:09:18 +0700

    final

--- calculator.py
+++ calculator.py
@@ -1,8 +1,40 @@
 import tkinter as tk
+reset_display = False
 
+def click(event):
+    global reset_display
 
-def click():
-    pass
+    current = entry.get()
+    button_text = event.widget["text"]
+
+    if button_text == 'C' :
+        entry.delete(0, tk.END)
+        entry.insert(tk.END, '0')
+        reset_display = False
+    
+    elif button_text == '=':
+        try: 
+            expression = current.replace('%', '/100')
+            result = eval(expression)
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, str(result))
+            reset_display = True
+
+        except Exception:
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, "Error!")
+            reset_display = True
+
+    else:
+        if reset_display:
+            entry.delete(0, tk.END)
+            current = ""
+            reset_display = False
+
+        if current == "0" and button_text.isdigit():
+            entry.delete(0, tk.END)
+        
+        entry.insert(tk.END, button_text)
 
 
 root = tk.Tk()
@@ -34,6 +66,7 @@ for i in range(len(buttons)):
         btn.grid(row=i, column=j, padx=10, pady=10)
         btn.configure(bg="#FF89CA")
         btn.bind('<Button-1>', click)
+
 
 root.mainloop()
 
